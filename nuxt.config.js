f5619adea9bb2e8ef227241618db1ba024cf617a
Herve <contact@herve-hochart.fr>
2019-02-22 18:00:39 +0100

    :tada: (:))

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const pkg = require('./package')
+require('dotenv').config()
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/tailwind.css'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: ['@nuxtjs/onesignal', '@nuxtjs/pwa'],
+
+  oneSignal: {
+    init: {
+      // cdn: true,
+      appId: process.env.ONE_SIGNAL_API_KEY,
+      allowLocalhostAsSecureOrigin: true,
+      welcomeNotification: {
+        disable: false
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        wco-pwa
+      </h1>
+      <h2 class="subtitle">
+        Weather Control Organization progressive web app
+      </h2>
+      <div class="links">
+        <a
+          class="button--green"
+        >Documentation</a>
+        <a
+          class="button--grey"
+          @click="send"
+        >GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    send () {
+      this.$OneSignal.push(() => {
+        this.$OneSignal.isPushNotificationsEnabled(isEnabled => {
+          if (isEnabled) {
+            alert('test')
+          } else {
+            alert('not enabled')
+          }
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  /* Sample `apply` at-rules with Tailwind CSS
+    .container {
+      @apply min-h-screen flex justify-center items-center text-center mx-auto;
+    }
+    */
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+</style>
