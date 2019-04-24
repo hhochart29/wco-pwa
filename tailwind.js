161291445795b4a2ae835bb3fb919cda7d81367f
Herve <contact@herve-hochart.fr>
2019-04-24 16:28:03 +0200

    :rocket:

--- tailwind.js
+++ tailwind.js
@@ -68,15 +68,7 @@ const colors = {
   indigo: '#6574cd',
   'indigo-light': '#7886d7',
   'indigo-lighter': '#b2b7ff',
-  'indigo-lightest': '#e6e8ff',
-
-  'purple-darkest': '#21183c',
-  'purple-darker': '#382b5f',
-  'purple-dark': '#794acf',
-  purple: '#9561e2',
-  'purple-light': '#a779e9',
-  'purple-lighter': '#d6bbfc',
-  'purple-lightest': '#f3ebff'
+  'indigo-lightest': '#e6e8ff'
 }
 
 module.exports = {
