2e73885d68af030c875b655fbe69d7f65ef07d4e
Herve <contact@herve-hochart.fr>
2019-05-22 00:02:28 +0200

    :lipstick: (adding articles icons)

--- components/ArticlePreview.vue
+++ components/ArticlePreview.vue
@@ -1,26 +1,49 @@
 <template>
-  <nuxt-link tag="article" :to="{name: 'article-id', params: { id: id } }" class="card">
-    <img :src="image.url" alt="image.alt">
-    <div>
-      <div>{{ title }}</div>
-      <p>{{ contentPreview }}</p>
+  <nuxt-link
+    tag="article"
+    :to="{name: 'article-id', params: { id: id } }"
+    class="card rounded overflow-hidden shadow-lg m-5 flex-grow"
+  >
+    <img :src="image.url" alt="image.alt" class="w-full">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ title }}</div>
+      <p class="text-grey-darker text-base">{{ contentPreview }}</p>
     </div>
-    <div>
-      <span>
-        <img :src="source.picture.url">
+    <div class="px-6 py-4 flex flex-wrap justify-between">
+      <span
+        class="inline-block bg-grey-light rounded-full px-3 py-1 my-1 text-sm font-semibold text-grey-darker"
+      >
+        <img :src="source.picture.url" class="w-6 rounded-full align-middle mr-2">
         {{ source.name }}
       </span>
-      <span>{{ date }}</span>
+      <span
+        class="inline-block bg-grey-light rounded-full px-3 py-1 my-1 text-sm font-semibold text-grey-darker"
+      >
+        <time-icon class="w-6 rounded-lg align-middle mr-2 fill-curent text-grey-darker"/>
+        {{ date }}
+      </span>
+      <span
+        class="inline-block bg-grey-light rounded-full px-3 py-1 my-1 text-sm font-semibold text-grey-darker"
+      >
+        <hour-glass  class="w-6 align-middle mr-2 fill-curent text-grey-darker" />
+        {{ readingTime }}mn
+      </span>
     </div>
   </nuxt-link>
 </template>
 
 <script>
+import timeIcon from '@/components/svg/time'
+import hourGlass from '@/components/svg/hourglass'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 
 export default {
   name: 'ArticlePreview',
+  components: {
+    timeIcon,
+    hourGlass
+  },
   props: {
     id: String,
     title: String,
@@ -37,7 +60,7 @@ export default {
       })
     },
     date () {
-      return dayjs(this.dateTime).locale('fr').format('Publié à HH:mm le D MMM YYYY')
+      return dayjs(this.dateTime).locale('fr').format('HH:mm, D MMM YYYY')
     },
     contentPreview () {
       return `${this.content.substr(0, 150)} ...`
@@ -48,29 +71,6 @@ export default {
 
 <style scoped lang="postcss">
 .card {
-  @apply rounded overflow-hidden shadow-lg m-5 flex-grow;
   flex-basis: 17rem;
-  & img {
-    @apply w-full;
-  }
-  & > div {
-    @apply px-6 py-4;
-    &:first-of-type {
-      & > div {
-        @apply font-bold text-xl mb-2;
-      }
-      & > p {
-        @apply text-grey-darker text-base;
-      }
-    }
-    &:last-of-type {
-      & > span {
-        @apply inline-block bg-grey-lighter rounded-full px-3 py-1 my-1 text-sm font-semibold text-grey-darker mr-2;
-        & > img {
-          @apply w-6 rounded-lg align-middle;
-        }
-      }
-    }
-  }
 }
 </style>
